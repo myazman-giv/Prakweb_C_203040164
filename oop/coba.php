c22a4488fab9d25d389239dacd441076af8e677a
Yazman <myazman9b@gmail.com>
2022-10-03 11:52:04 +0700

    membuat class dan object

--- oop/coba.php
+++ oop/coba.php
@@ -6,6 +6,8 @@ class Coba
 
 $a = new Coba();
 $b = new Coba();
+$c = new Coba();
 
 var_dump($a);
 var_dump($b);
+var_dump($c);
---
+++ oop/produk.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// manjual produk
+// komik
+// game
+
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+
+var_dump($produk1);
